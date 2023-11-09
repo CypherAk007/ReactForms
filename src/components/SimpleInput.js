7ebae0416a2a83c98e7f2e99dfb0ff0ded820709
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2023-11-09 16:48:51 +0530

    name field error handling like touched and sumbitted empty

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,28 +2,41 @@ import { useEffect, useState } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName,setEnteredName] = useState('')
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+  const [enteredNameTouched,setEnteredNameTouched] = useState(false);
 
   const nameInputChangeHandler = (event) =>{
     setEnteredName(event.target.value)
   }
   const onSubmitHandler = (event)=>{
     event.preventDefault()
+
+    // after the form is submitted then we consider that every feild is touched 
+    setEnteredNameTouched(true)
+
     // If we have empty feild then do nothing
     if (enteredName.trim()===''){
+      setEnteredNameIsValid(false)
       return
     }
+    
     console.log(enteredName);
     setEnteredName('')
   }
+
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
   useEffect(()=>{
     console.log(enteredName);
   },[enteredName])
 
+  const nameInputClasses = nameInputIsInvalid?'form-control invalid':'form-control'
   return (
     <form onSubmit={onSubmitHandler}>
-      <div className='form-control'>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input type='text' id='name' onChange={nameInputChangeHandler} value={enteredName}/>
+        {nameInputIsInvalid && <p className="error-text">Name must not be empty.</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
